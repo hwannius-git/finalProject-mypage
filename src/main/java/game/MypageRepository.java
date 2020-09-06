32339ffd0910b24f6f7a89f3e43efe600fb91a7d
hwannius-git <hwannamgung87@gmail.com>
2020-09-07 04:37:41 +0900

    1st commit

---
+++ src/main/java/game/MypageRepository.java
@@ -0,0 +1,14 @@
+package game;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MypageRepository extends CrudRepository<Mypage, Long> {
+
+    List<Mypage> findByMissionId(Long missionId);
+    List<Mypage> findByRewardId(Long rewardId);
+   // List<Mypage> findByRewardId(Long rewardId);
+
+}
